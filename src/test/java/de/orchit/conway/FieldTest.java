a8e123a1569b62b52930cd28e3adf057cd15af1b
pcornelissen <cornelis@pcornelissen.de>
2013-11-24 20:56:09 +0100

    refactoring introduced width&Height for the field

--- src/test/java/de/orchit/conway/FieldTest.java
+++ src/test/java/de/orchit/conway/FieldTest.java
@@ -8,7 +8,7 @@ public class FieldTest {
 
     @Test
     public void weCanInitAFieldThatConsistsOfDeadCells(){
-        final Field field = new Field();
+        final Field field = new Field(10, 15);
         for (Cell[] cells : field.getField()) {
             for(int i=0;i<cells.length;i++){
                 assertFalse(cells[i].isAlive());
@@ -18,7 +18,7 @@ public class FieldTest {
 
     @Test
     public void deadFieldIsAfterEvolveStillDead(){
-        Field field = new Field();
+        Field field = new Field(10, 15);
         field=field.evolve();
         for (Cell[] cells : field.getField()) {
             for(int i=0;i<cells.length;i++){
@@ -28,10 +28,14 @@ public class FieldTest {
     }
 
     private static class Field{
+        private final int width;
+        private final int height;
         private Cell [][] field;
 
-        private Field() {
-            this.field = new Cell[10][15];
+        private Field(int width, int height) {
+            this.width = width;
+            this.height = height;
+            this.field = new Cell[this.width][this.height];
             for (Cell[] cells : field) {
                 for(int i=0;i<cells.length;i++){
                     cells[i]=Cell.DEAD;
@@ -44,10 +48,10 @@ public class FieldTest {
         }
 
         public Field evolve() {
-            Field result = new Field();
+            Field result = new Field(width, height);
             for(int x=0;x<result.getField().length;x++){
                 Cell[] cells=result.getField()[x];
-                for(int y=0;y<cells.length;i++){
+                for(int y=0;y<cells.length;y++){
                     cells[y]=field[x][y].evolve(0);
                 }
             }
