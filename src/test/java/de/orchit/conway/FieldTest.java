f4e0ef38252faae12caeb8d3f6dbb60e8e21db7e
pcornelissen <cornelis@pcornelissen.de>
2013-11-24 20:35:46 +0100

    started with field tests

---
+++ src/test/java/de/orchit/conway/FieldTest.java
@@ -0,0 +1,12 @@
+package de.orchit.conway;
+
+import org.junit.Test;
+
+public class FieldTest {
+    @Test
+    public void weCanInitAField(){
+
+        Cell [][] field = new Cell[10][15];
+
+    }
+}
