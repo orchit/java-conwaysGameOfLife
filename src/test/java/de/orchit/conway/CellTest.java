8793976e6790612086a6575c96ddbc0c6d4a59c2
pcornelissen <cornelis@pcornelissen.de>
2013-11-24 20:32:24 +0100

    extracted inner class

--- src/test/java/de/orchit/conway/CellTest.java
+++ src/test/java/de/orchit/conway/CellTest.java
@@ -78,25 +78,4 @@ public class CellTest {
         assertFalse(cell.evolve(4).isAlive());
     }
 
-    private static class Cell {
-        public static Cell LIVING = new Cell(true);
-        public static Cell DEAD = new Cell(false);
-
-        private boolean alive;
-
-        private Cell(boolean isAlive) {
-            alive = isAlive;
-        }
-
-
-        public boolean isAlive() {
-            return alive;
-        }
-
-        public Cell evolve(int neighborCount) {
-            if(neighborCount<=1||neighborCount>3) return Cell.DEAD;
-            if(alive==false && neighborCount ==2) return Cell.DEAD;
-            return Cell.LIVING;
-        }
-    }
 }
