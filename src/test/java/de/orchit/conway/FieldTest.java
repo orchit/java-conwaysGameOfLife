cc60ce7aea05c4ac8b3ece6080eec1d5785880c6
pcornelissen <cornelis@pcornelissen.de>
2013-11-24 20:37:39 +0100

    field is initially dead

--- src/test/java/de/orchit/conway/FieldTest.java
+++ src/test/java/de/orchit/conway/FieldTest.java
@@ -4,9 +4,13 @@ import org.junit.Test;
 
 public class FieldTest {
     @Test
-    public void weCanInitAField(){
+    public void weCanInitAFieldThatConsistsOfDeadCells(){
 
         Cell [][] field = new Cell[10][15];
-
+        for (Cell[] cells : field) {
+            for(int i=0;i<cells.length;i++){
+                cells[i]=Cell.DEAD;
+            }
+        }
     }
 }
