a4d3ebf98140a32b0b24db6af689c6eba1021996
pcornelissen <cornelis@pcornelissen.de>
2013-11-24 20:09:08 +0100

    cells can be dead

--- src/test/java/de/orchit/conway/CellTest.java
+++ src/test/java/de/orchit/conway/CellTest.java
@@ -1,17 +1,38 @@
 package de.orchit.conway;
 
 
+import org.junit.Test;
+
 import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class CellTest {
 
+    @Test
     public void weHaveLivingCells(){
         assertTrue(new Cell().isAlive());
     }
 
+    @Test
+    public void weHaveDeadCells(){
+        assertFalse(new Cell(false).isAlive());
+    }
+
     private static class Cell {
+
+        private boolean alive;
+
+        private Cell() {
+            alive = true;
+        }
+
+        public Cell(boolean isAlive) {
+            alive=isAlive;
+        }
+
+
         public boolean isAlive() {
-            return true;
+            return alive;
         }
     }
 }
