be28bbc1c63d9769c7da24c4df548a1bd5a6db8c
pcornelissen <cornelis@pcornelissen.de>
2013-11-24 20:50:45 +0100

    refactored array into class

--- src/test/java/de/orchit/conway/FieldTest.java
+++ src/test/java/de/orchit/conway/FieldTest.java
@@ -2,15 +2,34 @@ package de.orchit.conway;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
+
 public class FieldTest {
+
     @Test
     public void weCanInitAFieldThatConsistsOfDeadCells(){
-
-        Cell [][] field = new Cell[10][15];
-        for (Cell[] cells : field) {
+        final Field field = new Field();
+        for (Cell[] cells : field.getField()) {
             for(int i=0;i<cells.length;i++){
-                cells[i]=Cell.DEAD;
+                assertFalse(cells[i].isAlive());
             }
+        }
+    }
+
+    private static class Field{
+        private Cell [][] field;
+
+        private Field() {
+            this.field = new Cell[10][15];
+            for (Cell[] cells : field) {
+                for(int i=0;i<cells.length;i++){
+                    cells[i]=Cell.DEAD;
+                }
+            }
+        }
+
+        private Cell[][] getField() {
+            return field;
         }
     }
 }
